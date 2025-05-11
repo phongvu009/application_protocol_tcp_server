b22fd8e1402f62bd05f0544534cdea6a2ea0c772
phong vu <kimphong90@hotmail.com>
2025-05-10 21:44:53 -0500

    parsing message

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket 
 import struct 
+import json
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -25,8 +26,23 @@ def run_server(host,port):
                     #! : !Network byte order, 
                     #H : 2bytes-16bits, unsigned short, in range 0x0000 to 0xFFFF or 0 to 65535.
                     #!H : Universal Network byte order, 
-                    json_header_length = struct.unpack('!H', fixed_length_header)
+                    json_header_length  = struct.unpack('!H', fixed_length_header)[0]
                     logging.info(f"JSON header length: {json_header_length}")
+                    #read JSON header
+                    json_header = conn.recv(json_header_length) # sever need to know exactly how many bytes to read
+                    json_header = json.loads(json_header.decode('utf-8')) # convert raw byte sequence into json string, then convert into python dictionary
+                    logging.debug(f"JSON header: {json_header}")
+                    #parse JSON header
+                    content_length = json_header['content_length']
+                    content_type = json_header['content_type']
+                    content_encoding = json_header['content-encoding']
+
+                    #read message content
+                    message_content = conn.recv(content_length) # read next chunk
+                    if content_encoding == 'utf-8':
+                        message_content = message_content.decode('utf-8')
+                    logging.info(f"Message content: {message_content}")
+                    logging.info(f"Content type: {content_type}, Encoding: {content_encoding}")
     
 
 if __name__ == "__main__":
